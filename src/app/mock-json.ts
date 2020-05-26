0423716139bd4fd8733d8f14c17acd5a299bb443
samujwala <samujwala13@gmail.com>
2020-05-26 08:51:55 +0400

    2 files modified

--- src/app/mock-json.ts
+++ src/app/mock-json.ts
@@ -12,5 +12,17 @@ export const cryptocurrencyList = [
   { priceUsd: 6.9823147841833210, name: 'EOS' 
   },
   { priceUsd: 0.2341611226032443, name: 'Stellar'
+  },
+  { priceUsd: 0.7046774500729512, name: 'bibox-token'
+  },
+   { priceUsd: 10.0684816129771492, name: 'Elastos'
+  },
+   { priceUsd: 0.1059951144518822, name: 'CyberMiles'
+  },
+  { priceUsd: 0.1118098937633353, name: 'Theta Token'
+  },
+   { priceUsd: 0.7060400981262156, name: 'TenX'
+  },
+   { priceUsd: 0.1324975355943374, name: 'Loom Network'
   }
 ];
